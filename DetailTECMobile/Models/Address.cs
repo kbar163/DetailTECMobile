438e8d003227bff8d6864263e84060b420e54a0b
kbar163 <kabarrantes163@gmail.com>
2022-10-20 04:06:50 -0600

    Creacion de modelos de datos
    Se crean los modelos de datos Customer y Address para el manejo de clientes.

---
+++ DetailTECMobile/Models/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace DetailTECMobile.Models
+{
+    public class Address
+    {
+        public string provincia { get; set; }
+        public string canton { get; set; }
+        public string distrito { get; set; }
+    }
+}
