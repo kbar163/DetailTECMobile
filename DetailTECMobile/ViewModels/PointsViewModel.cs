fc32a5b87c83b1d43f989eb70b1e2ce80e4ad640
kbar163 <kabarrantes163@gmail.com>
2022-10-28 00:40:04 -0600

    Cambios points view

--- DetailTECMobile/ViewModels/PointsViewModel.cs
+++ DetailTECMobile/ViewModels/PointsViewModel.cs
@@ -5,14 +5,43 @@ using System.Text;
 
 namespace DetailTECMobile.ViewModels
 {
-    public class PointsViewModel : BaseViewModel
+    public class PointsViewModel
     {
+        #region Atributes
         private Customer user = App.loggedUser;
-        
+        #endregion
+
+        #region Properties
+        public string AccumulatedPoints
+        {
+            get
+            {
+                return this.user.puntos_acum.ToString();
+            }
+        }
+
+        public string ObtainedPoints
+        {
+            get
+            {
+                return this.user.puntos_obt.ToString();
+            }
+        }
+
+        public string RedeemedPoints
+        {
+            get
+            {
+                return this.user.puntos_redim.ToString();
+            }
+        }
+
+        #endregion
+
         #region Constructor
         public PointsViewModel()
         {
-            
+
         }
         #endregion
 
